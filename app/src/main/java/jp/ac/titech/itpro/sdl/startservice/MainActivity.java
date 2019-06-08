9b2dc38aa68de652240ac9b4ff97c978231983f9
Jeshan Air <jasondangol>
2019-06-08 11:26:43 +0900

    Toast msg added

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,19 +1,50 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    private BroadcastReceiver my_receiver;      // to receive from Service3
+    private IntentFilter my_filter;             // to register receiver
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+
+        my_receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+
+                if (action == null)
+                    return;
+                else if (action.contentEquals("My Custom Msg")) {
+//                    Log.d(TAG, "Hurrah, I (MainActivity) received sth of action:" + action);
+                    Bundle bundle = intent.getExtras();
+                    if (bundle != null) {
+                        Toast.makeText(context,bundle.getString("Service3_payload"), Toast.LENGTH_LONG).show();
+                        Log.d(TAG, bundle.getString("Service3_payload"));
+                    }
+                }
+            }
+        };
+
+        my_filter = new IntentFilter();
+        my_filter.addAction("My Custom Msg");
+        this.registerReceiver(my_receiver,my_filter);
     }
 
     public void onClickTest1(View v) {
@@ -27,6 +58,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onClickTest2 in " + Thread.currentThread());
         Intent intent = new Intent(this, Service2.class);
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
+    public void onClickTest3(View view) {
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG,"Hello, Service3");
         startService(intent);
     }
 }
--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/Service3.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/Service3.java
@@ -1,4 +1,53 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
-public class Service3 {
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class Service3 extends IntentService {
+    private final static String TAG = Service3.class.getSimpleName();
+    public static String EXTRA_MYARG = "MYARG" ;
+    private Intent my_intent;
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name [i removed name] Used to name the worker thread, important only for debugging.
+     */
+    public Service3() {
+        super(TAG);
+    }
+
+    //https://developer.android.com/guide/components/services#java
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // Normally we would do some work here, like download a file.
+        // For our sample, we just sleep for 5 seconds.
+        Log.d(TAG, "onHandleIntent in " + Thread.currentThread());
+        Log.d(TAG, "myarg = " + intent.getStringExtra(EXTRA_MYARG));
+        try {
+            Thread.sleep(5000); // 5 sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        my_intent = new Intent();
+        my_intent.setAction("My Custom Msg");
+        my_intent.putExtra("Service3_payload", "I'm Service3. Thanks for choosing me, MainActivity!");
+        sendBroadcast(my_intent);
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy in " + Thread.currentThread());
+    }
+
+
 }
